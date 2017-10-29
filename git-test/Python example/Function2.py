b318479f183b18ffe97d98cba04aaadfea0949d7
Kita29 <bagomedova.alina@gmail.com>
2017-10-29 23:08:01 +0300

    add new function for calculation average

--- git-test/Python example/Function2.py
+++ git-test/Python example/Function2.py
@@ -8,3 +8,8 @@ def func(a, b=5, c=10):
 #           func(3, 7) - a=3,b=7,c=10
 #           func(25, c=24) - a=25,b=5,c=24
 #           func(c=50, a=100) - a=100, b=5, c50
+
+def aver(val1, val2=5, val3=10):
+    print('The average is ', (val1 + val2 + val3) / 3)
+    
+    
