759a03c38a3157d78ddf351b4ea6cd9fe0dec815
Kita29 <bagomedova.alina@gmail.com>
2017-10-29 22:48:53 +0300

    chance file function2

--- git-test/Python example/Function2.py
+++ git-test/Python example/Function2.py
@@ -1,5 +1,7 @@
-def summa(x, y):
+def summa(x, y = 2):
     return x + y
 
-f = summa
-v = f(10, 3) # call function summa across function f
+a = summa(3) # x = 3, y = default
+b = summa(10, 40) # change second value
+print (a) 
+print (b)
