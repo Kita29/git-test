06dac0200f4b74a835582631b4fa45bd0995b9f3
Kita29 <bagomedova.alina@gmail.com>
2017-10-29 23:15:48 +0300

    add function with locking

--- git-test/Python example/Internal_function.py
+++ git-test/Python example/Internal_function.py
@@ -2,3 +2,9 @@ def outer(a, b):
     def inner(c, d):
         return c + d
     return inner(a, b)
+
+def kn(a):
+    def inner():
+        return a + 5
+    return inner
+# example: a = kn(6) ---> a()
