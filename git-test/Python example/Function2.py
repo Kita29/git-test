603e780182f907d45e9dd7c1cf2ccc230133894e
Kita29 <bagomedova.alina@gmail.com>
2017-10-29 22:59:43 +0300

    change of parameters

--- git-test/Python example/Function2.py
+++ git-test/Python example/Function2.py
@@ -1,7 +1,10 @@
-def summa(x, y):
-    return x + y
+#def summa(x, y):
+#    return x + y
 
-def func(f, a, b):
-    return f (a, b)
+def func(a, b=5, c=10):
+    print ('a = ', a, ' b = ', b, 'c = ', c)
 
-# for example v = func(summa, 10 ,3) - variable summa as an argument
+# for example:
+#           func(3, 7) - a=3,b=7,c=10
+#           func(25, c=24) - a=25,b=5,c=24
+#           func(c=50, a=100) - a=100, b=5, c50
