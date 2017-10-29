87c22c6349c7c54ccf08d1c0cd4b7cd5a40a403f
Kita29 <bagomedova.alina@gmail.com>
2017-10-29 22:52:46 +0300

    function as an argument

--- git-test/Python example/Function2.py
+++ git-test/Python example/Function2.py
@@ -1,7 +1,7 @@
-def summa(x, y = 2):
+def summa(x, y):
     return x + y
 
-a = summa(3) # x = 3, y = default
-b = summa(10, 40) # change second value
-print (a) 
-print (b)
+def func(f, a, b):
+    return f (a, b)
+
+# for example v = func(summa, 10 ,3) - variable summa as an argument
