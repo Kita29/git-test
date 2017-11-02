b01bb5817f93c55d901edfde705b0d5eced6e7b4
Kita29 <bagomedova.alina@gmail.com>
2017-11-02 21:37:47 +0300

    add new file with work on string

---
+++ git-test/Python example/string.py
@@ -0,0 +1,19 @@
+def string():
+    print('hello')
+    print(len('Hello')) # sum letters
+    print('Hello' + ' my friend') # join rows
+    print('Hello' + str(5)) # convert numeric at string
+    print("SPAM" * 10) # repeat
+    s = "I am student "
+    print(s)
+    print(s * 4)
+    print('Hello\'s')
+    print("Hello's")
+    # \n - go to new string
+    # \t - tabulation
+    # \\ - insert \
+    # \' - insert '
+    # \" - insert "
+    print('This is very \n big string')
+    print(1, 4, 6, sep=':')
+    
