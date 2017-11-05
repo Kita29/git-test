207bea379536d189036f9c7407206159b81daeb3
Kita29 <bagomedova.alina@gmail.com>
2017-11-05 11:29:09 +0300

    cost of conversation

---
+++ git-test/Python example/Cost_of_conversation.py
@@ -0,0 +1,11 @@
+def talk(code_city, duration_of_conversation):
+    if (code_city == 343):
+        print("Cost of conversation: ", duration_of_conversation * 15)
+    elif (code_city == 381):
+        print("Cost of conversation: ", duration_of_conversation * 18)
+    elif (code_city == 473):
+        print("Cost of conversation: ", duration_of_conversation * 13)
+    elif (code_city == 485):
+        print("Cost of conversation: ", duration_of_conversation * 11)
+    else:
+        print("I don't know")
