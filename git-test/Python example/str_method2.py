cb61ca3e4dc076df2979ac0f1b7e07c1f4ac10cb
Kita29 <bagomedova.alina@gmail.com>
2017-11-26 15:47:51 +0300

    not completed

---
+++ git-test/Python example/str_method2.py
@@ -0,0 +1,17 @@
+def str_method_exampl():
+    s = "If you add a USB controller to a virtual machine in the VMware Host Client version 1.8, you cannot remove the USB controller."
+    print("s = ", s)
+    print(s.count('o'))
+    #'O' + s[2:]
+    #print(s)
+    if (s.isalpha() == False):
+        print(s.upper())
+    if len(s) > 4:
+        print(s.lower)
+    'O' + s[1:]
+    print(s)
+   # c = len(s)
+    #k = 0
+    #v = s.find('o',k,c)
+    #print(s.find('o',k,c))
+    #k = v
