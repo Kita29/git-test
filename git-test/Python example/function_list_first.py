f7b731730643e46b1f29f7df252348b78e2647e8
Kita29 <bagomedova.alina@gmail.com>
2018-02-11 22:00:11 +0300

    studied copy list

--- git-test/Python example/function_list_first.py
+++ git-test/Python example/function_list_first.py
@@ -21,3 +21,30 @@ del a[2:4]
 print("del a[2:4] -> a = ", a)
 del a[:]
 print("a[:] = ", a)
+print("_____")
+print("h = ", h)
+p = h
+print("p = ", p)
+p[0] = 1
+print("p[0] = 1")
+print("h = ", h)
+print("p = ", p)
+print("_____")
+x = y = [1, 2]
+print("x is y = ", x is y)
+x = [1, 2]
+y = [1, 2]
+print("x is y = ", x is y)
+print("_____")
+a = [4, 3, [2, 1]]
+b = a[:]
+print("b is a = ", b is a)
+b[2][0] = -100
+print("b[2][0] = -100")
+print("a = ", a)
+print("_____")
+import copy
+a = [4, 3, [2, 1]]
+b = copy.deepcopy(a)
+b[2][0] = -100
+print("a = ", a)
