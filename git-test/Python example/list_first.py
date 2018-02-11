aa2a120e82e56bcf1a32817cd8b8fd07209a1ffd
Kita29 <bagomedova.alina@gmail.com>
2018-02-11 21:14:10 +0300

    working with list

---
+++ git-test/Python example/list_first.py
@@ -0,0 +1,22 @@
+e = [56.8060, 57.1578, 57.4093, 56.1843, 57.2207]
+print("e = ",e)
+print("e[0] = ",e[0])
+print("e[1] = ",e[1])
+print("e[-1] = ",e[-1])
+h = ['Hi', 27, -8.1, [1, 2]]
+print("h = ", h)
+h[1] = 'Hello'
+print("h = ", h)
+print("h[1] = ", h)
+h = ['bonjour', 'privet', 'hola', 'aloha', 'hi']
+print("h = ", h)
+h[1] = 'Hello'
+print("h = ", h)
+print("_______")
+print("e = ",e)
+print("len(e) = ", len(e))
+print("max(e) = ", max(e))
+print("min(e) = ", min(e))
+print("sum(e) = ", sum(e))
+print("sorted(e) = ", sorted(e))
+print("e = ",e)
