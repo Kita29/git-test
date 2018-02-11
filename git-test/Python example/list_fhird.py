a55dae80cd4a5ffc50630b84128c2c07be5fa7ed
Kita29 <bagomedova.alina@gmail.com>
2018-02-11 22:06:30 +0300

    different srezy

---
+++ git-test/Python example/list_fhird.py
@@ -0,0 +1,8 @@
+L = [3, 'hello', 7, 4, 'privet', 4, 3, -1]
+print("L = ", L)
+print("L[:3] = ", L[:3])
+print("L[:] = ", L[:])
+print("L[::2] = ", L[::2])
+print("L[::-1] = ", L[::-1])
+print("L[:-1] = ", L[:-1])
+print("L[-1:] = ", L[-1:])
