c4e9b3b78d12d11a20650545c4cb9e50abb7e855
Kita29 <bagomedova.alina@gmail.com>
2018-02-11 21:26:45 +0300

    studied operator in

---
+++ git-test/Python example/function_list_first.py
@@ -0,0 +1,11 @@
+def f(x,y):
+    print(x+y)
+
+f([1, 2, 3], [4, 5, 6])
+f("123", "456")
+f(1,2)
+
+h = ['bonjour', 7, 'hola', -1.0, 'privet']
+print("h = ", h)
+if 7 in h:
+    print("7 included in list")
