fee59f80156483b1af1272417a41aee99758c476
Kita29 <bagomedova.alina@gmail.com>
2018-02-11 21:48:01 +0300

    add example with a srez

--- git-test/Python example/function_list_first.py
+++ git-test/Python example/function_list_first.py
@@ -9,3 +9,15 @@ h = ['bonjour', 7, 'hola', -1.0, 'privet']
 print("h = ", h)
 if 7 in h:
     print("7 included in list")
+print("_____")
+g = h[1:2]
+print("h[1:2] = ", g)
+print("_____")
+a = [-1, 1, 66.25, 333, 333, 1234.5]
+print("a = ", a)
+del a[0]
+print("del a[0] -> a = ", a)
+del a[2:4]
+print("del a[2:4] -> a = ", a)
+del a[:]
+print("a[:] = ", a)
